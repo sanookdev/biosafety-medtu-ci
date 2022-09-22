f1445ac875c458f9f24b5f186afb5213af610b30
Mr.Nuk <supaporn.dev@gmail.com>
2022-09-22 09:42:44 +0700

    e

--- application/views/login_view.php
+++ application/views/login_view.php
@@ -9,7 +9,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Page</title>
+    <title>Login Paged</title>
 </head>
 
 <body>
